ca1e14bf1ede6f6dd6c3ab75cf490888b9a12bb9
ollie-2 <135734430+ollie-2@users.noreply.github.com>
2024-06-03 08:41:50 -0400

    Added new ticket sprites/fixed ticket gen

--- ticket.py
+++ ticket.py
@@ -3,10 +3,15 @@ import pygame
 
 class Ticket:
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, mode):
         self.x = x
         self.y = y
         self.image = pygame.image.load("Ticket.png")
+        self.image_list = [
+            "Ticket.png",
+            "Ticket_invalid.png",
+            "Ticket_valid.png"
+        ]
         self.rescale_image(self.image)
         self.image_size = self.image.get_size()
         self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
@@ -15,4 +20,9 @@ class Ticket:
         self.image_size = self.image.get_size()
         scale_size = (self.image_size[0] * 12, self.image_size[1] * 12)
         self.image = pygame.transform.scale(self.image, scale_size)
+
+    def switch_image(self, mode):
+        self.image = pygame.image.load(self.image_list[mode])
+        self.rescale_image(self.image)
+        self.image_size = self.image.get_size()
 
